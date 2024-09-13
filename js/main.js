0a39dab65e29234f1ed10d35e9dd16eaab7253cb
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-09-13 23:58:21 +0300

    Add A Three Sections With There JS Data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 /** @format */
+// ! Yearly Target Progress Bar Fill When Appear
+const skills = document.querySelector(".yearly-target .prog");
+const allSkills = document.querySelectorAll(".prog .details .progress span");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= skills.offsetTop) {
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.prog;
+    });
+  } else {
+    allSkills.forEach((skill) => {
+      skill.style.width = "0";
+    });
+  }
+});
+
+// ! start count Of Tickets Statistics
+let stats = document.getElementById("statistics");
+let number = document.querySelectorAll(".row .box span");
+let start = false;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= stats.offsetTop) {
+    if (!start) {
+      number.forEach((num) => startCount(num));
+    }
+    start = true;
+  }
+});
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
